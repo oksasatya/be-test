8d367297bc9c9b81bb149a493a20efb194bfdeae
oksa satya <oksasatyaa@gmail.com>
2025-08-04 10:43:32 +0400

    first init

--- prisma/seeds/index.ts
+++ prisma/seeds/index.ts
@@ -1,6 +1,9 @@
+import {seedAdmin} from "./seedAdmin";
+import {prisma} from "$utils/prisma.utils";
+
 async function seed(){
     // Seed Function Call Goes Here
-   
+   await seedAdmin(prisma)
 }
 
 seed().then(()=>{
